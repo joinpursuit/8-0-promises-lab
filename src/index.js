c674c7fbbef096fd2e5b4ea0d2d11ba2d13a92db
Stephanie <stephaniereyes@pursuit.org>
2021-09-30 16:49:19 -0400

    finished lab

--- src/index.js
+++ src/index.js
@@ -7,10 +7,22 @@ const finder = require("./helpers/finder");
  * Logs out the entire array of objects returned from the `finder()` function.
  *
  * @param {Object} person - A person's names.
- * @param {Object} person.first - The person's first name.
- * @param {Object} person.last - The person's last name.
+ * @param {String} person.first - The person's first name.
+ * @param {String} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  
+  finder(person.first,person.last)
+   .then((data)=>{
+     console.log(data)
+   })
+   .catch((error)=>{
+     console.log(error)
+   })
+}
+ 
+
+
 
 /**
  * logTwoResults()
@@ -22,7 +34,26 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1,person2) {
+  finder(person1.first,person1.last)
+   .then((data)=>{
+     console.log(data)
+   })
+   .catch((error)=>{
+     console.log(error)
+   })
+   finder(person2.first,person2.last)
+   .then((data)=>{
+     console.log(data)
+   })
+   .catch((error)=>{
+     console.log(error)
+   })
+}
+ 
+
+
+
 
 /**
  * logThreeResultsCities()
@@ -35,7 +66,29 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1,person2,person3) {
+  finder(person1.first, person1.last)
+  .then((value)=>{
+    console.log(value[0].city)
+  })
+  .catch((error)=>{
+    console.log(error)})
+   
+    finder(person2.first, person2.last)
+  .then((value)=>{
+    console.log(value[0].city)
+  })
+  .catch((value)=>{
+    console.log(value)})
+
+    finder(person3.first, person3.last)
+  .then((value)=>{
+    console.log(value[0].city)
+  })
+  .catch((error)=>{
+    console.log(error)})
+
+}
 
 // Do not change any of the code below this line.
 module.exports = {
