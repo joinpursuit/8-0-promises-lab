f9d979b03020eae40a10a00a9fa9838d1996ccf0
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-09-30 14:33:37 -0400

    adds logsTwoResults

--- src/index.js
+++ src/index.js
@@ -26,7 +26,29 @@ finder(first, last)
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+// let arr = function.arguments;
+// arr.forEach(({first, last}) => {
+// finder(first, last)
+// .then()
+// })
+finder(person1.first, person1.last)
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+  
+finder(person2.first, person2.last)
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
 
 /**
  * logThreeResultsCities()
