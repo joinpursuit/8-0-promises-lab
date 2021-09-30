e35052475c76b226f516b364ac38af1b1f9cffb7
DylanLovell <arcanyne7@gmail.com>
2021-09-30 13:37:16 -0400

    finished problems

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Do not change the line directly below.
-const finder = require("./helpers/finder");
+const finder = require('./helpers/finder');
 
 /**
  * logResult()
@@ -10,7 +10,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+const logResult = (person) => {
+  finder(person.first, person.last)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 /**
  * logTwoResults()
@@ -22,8 +30,9 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
-
+function logTwoResults(person1, person2) {
+  [...arguments].forEach((e) => logResult(e));
+}
 /**
  * logThreeResultsCities()
  * ========================
@@ -35,8 +44,20 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  [...arguments].forEach((e) => logResultCity(e));
+}
 
+//helper function
+const logResultCity = (person) => {
+  finder(person.first, person.last)
+    .then((result) => {
+      console.log(result[0].city);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 // Do not change any of the code below this line.
 module.exports = {
   logResult,
