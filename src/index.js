d03540d3ba54079e27b0c3e6d8e31370ba2a4532
Jalamang <Jalamangjanka@gmail.com>
2021-09-30 17:58:16 -0400

    logged all cities

--- src/index.js
+++ src/index.js
@@ -58,9 +58,33 @@ function logTwoResults(person1, person2) {
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
 function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last)
+    .then((person1) => {
+      finder(person2.first, person2.last)
+        .then((person2) => {
+          finder(person3.first, person3.last)
+            .then((person3) => {
+              if (person1, person2, person3) {
+                console.log(`${person1[0].city}`)
+              }
+              if (person2) {
 
-  
+                console.log(`${person2[0].city}`)
+              }
+              if (person3) {
 
+                console.log(`${person3[0].city}`)
+              }
+
+            }).catch((error) => {
+              console.log(`${error}`)
+            })
+        }).catch((error) => {
+          console.log(`${error}`)
+        })
+    }).catch((error) => {
+      console.log(`${error}`)
+    })
 }
 
 // Do not change any of the code below this line.
