de51dcaeca3427403f46cc87330da1d7c453f469
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-30 12:42:38 -0400

    Arrow Functions Galore

--- src/index.js
+++ src/index.js
@@ -1,57 +1,26 @@
-// Do not change the line directly below.
 const finder = require("./helpers/finder");
 
-/**
- * logResult()
- * ========================
- * Logs out the entire array of objects returned from the `finder()` function.
- *
- * @param {Object} person - A person's names.
- * @param {Object} person.first - The person's first name.
- * @param {Object} person.last - The person's last name.
- */
-function logResult({first, last}) {
+const logResult = ({ first, last }) => {
   finder(first, last)
     .then((val) => console.log(val))
     .catch((err) => console.log(err));
 };
 
-/**
- * logTwoResults()
- * ========================
- * Logs out the entire array of objects returned from the `finder()` function for two different people.
- *
- * @param {Object} person1 - A person's names.
- * @param {Object} person1.first - The person's first name.
- * @param {Object} person1.last - The person's last name.
- * @param {Object} person2 - A person's names. In the same format as person1.
- */
-function logTwoResults(...obj) {
-  obj.forEach(({first, last}) => {
+const logTwoResults = (...obj) => {
+  obj.forEach(({ first, last }) => {
     finder(first, last)
-    .then((val) => console.log(val))
-    .catch((err) => console.log(err));
+      .then((val) => console.log(val))
+      .catch((err) => console.log(err));
   });
 };
 
-/**
- * logThreeResultsCities()
- * ========================
- * Logs out just the cities from all three people given to the function. You may assume that each search result will only return a single person.
- *
- * @param {Object} person1 - A person's names.
- * @param {Object} person1.first - The person's first name.
- * @param {Object} person1.last - The person's last name.
- * @param {Object} person2 - A person's names. In the same format as person1.
- * @param {Object} person3 - A person's names. In the same format as person1.
- */
-function logThreeResultsCities(...obj) {
-  obj.forEach(({first, last}) => {
+const logThreeResultsCities = (...obj) => {
+  obj.forEach(({ first, last }) => {
     finder(first, last)
-    .then(([{city}]) => console.log(city))
-    .catch((err) => console.log(err));
-});
-}
+      .then(([{ city }]) => console.log(city))
+      .catch((err) => console.log(err));
+  });
+};
 
 // Do not change any of the code below this line.
 module.exports = {
