0c1e5dcf74bff965c7c45ed75089cfa14a4cd5d8
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-09-30 17:19:52 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -10,24 +10,45 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last)
+  .then((eachResult) => {
+    console.log(eachResult);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
 
 /**
  * logTwoResults()
  * ========================
- * Logs out the entire array of objects returned from the `finder()` function for two different people.
+ * Logs out the entire array of objects returned from the `finder()` 
+ * function for two different people.
  *
  * @param {Object} person1 - A person's names.
  * @param {Object} person1.first - The person's first name.
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last).then((eachResult) => {
+    console.log(eachResult);
+
+    finder(person2.first, person2.last).then((person2Result) => {
+      console.log(person2Result);
+    })
+    .catch((error) => console.log(error));
+
+  })
+  .catch((error) => console.log(error));
+}
 
 /**
  * logThreeResultsCities()
  * ========================
- * Logs out just the cities from all three people given to the function. You may assume that each search result will only return a single person.
+ * Logs out just the cities from all three people given to the function. 
+ * You may assume that each search result will only return a single person.
  *
  * @param {Object} person1 - A person's names.
  * @param {Object} person1.first - The person's first name.
@@ -35,7 +56,23 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last)
+  .then((result) => {
+    console.log(result[0].city);
+
+    finder(person2.first, person2.last)
+    .then((result2) => {
+      console.log(result2[0].city);
+    }).catch((error) => console.log(error));
+
+    finder(person3.first, person3.last)
+    .then((result3) => {
+      console.log(result3[0].city);
+    }).catch((error) => console.log(error));
+
+  }).catch((error) => console.log(error));
+}
 
 // Do not change any of the code below this line.
 module.exports = {
