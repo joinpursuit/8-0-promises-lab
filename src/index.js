114b0e9da7f6e30fd6912ec4b02ab4cab4f9ccc7
salrey <tekdehn@gmail.com>
2021-09-30 12:40:15 -0400

    Logs out the entire array of objects returned from from finder function

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person, first = person.first, last = person.last) {
+  finder(first, last)
+  .then((person) => {
+    console.log(person);
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}
 
 /**
  * logTwoResults()
