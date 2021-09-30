9c0fef6d145709cfcdf5b88771678285e61eb4ee
vanessa conti <vanessaconti@pursuit.org>
2021-09-30 17:57:04 -0400

    finished lab

--- src/index.js
+++ src/index.js
@@ -55,7 +55,13 @@ function logTwoResults(person1, person2) {
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(...cities) {
+  cities.forEach(({ first, last }) => {
+    finder(first, last)
+      .then(([{ city }]) => console.log(city))
+      .catch((error) => console.log(error));
+  });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
