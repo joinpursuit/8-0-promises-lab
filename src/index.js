22f771857918e930859b69a1268b6064648b808d
Kiyomi Rodgers Denniston <kiyomi.rodgersdenniston@gmail.com>
2021-09-30 16:28:07 -0400

    finished:Deborah Parner

--- src/index.js
+++ src/index.js
@@ -10,7 +10,29 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+
+//  const position = 3;
+//  let thirdPlace = null;
+//  getParticipantAtPosition(position)
+//    .then((name) => {
+//      thirdPlace = name;
+//    })
+//    .catch((error) => {
+//      console.log(`ERROR: ${error}`);
+//    });
+ 
+//  console.log(`${thirdPlace} came in third place!`);
+
+// promise1.then((value) => {
+//   console.log(value);
+function logResult(person) {
+//a function that invokes a promise
+  finder(person.first,person.last)
+//value is the value of finder
+    .then((value) => {console.log(value)})
+  // finder(first, last)
+    .catch((error) => {console.log(error)});
+}
 
 /**
  * logTwoResults()
@@ -22,7 +44,15 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1,person2) {
+  finder(person1.first, person1.last)
+    .then((value) => {console.log(value)})
+    .catch((error) => {console.log(error)});
+    
+  finder(person2.first, person2.last)
+    .then((value) => {console.log(value)})
+    .catch((error) => {console.log(error)});
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +65,19 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1,person2,person3) {
+  finder(person1.first, person1.last)
+    .then((value) => {console.log(value[0].city)})
+    .catch((error) => {console.log(error)});
+    
+  finder(person2.first, person2.last)
+    .then((value) => {console.log(value[0].city)})
+    .catch((error) => {console.log(error)});
+
+  finder(person3.first, person3.last)
+    .then((value) => {console.log(value[0].city)})
+    .catch((error) => {console.log(error)});
+}
 
 // Do not change any of the code below this line.
 module.exports = {
