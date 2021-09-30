fbef36d97aa0b7750dc33d05666b75adf488e084
Mukayila ALAO <mukayilaalao@pursuit.org>
2021-09-30 14:40:51 -0400

    Lab tasks done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  let promise=finder(person.first, person.last);
+  promise
+  .then(newArr=> console.log(newArr))
+  .catch(err => {
+     console.log(err);
+  })
+}
 
 /**
  * logTwoResults()
@@ -22,7 +29,15 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  let promise1=finder(person1.first, person1.last);
+  let promise2=finder(person2.first, person2.last);
+  Promise.all([promise1,promise2]).then(newArr=>{
+    for(let arr of newArr) console.log(arr);
+  }).catch(err=> {
+    console.log(err);
+  });
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +50,18 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  let promise1=finder(person1.first, person1.last);
+  let promise2=finder(person2.first, person2.last);
+  let promise3=finder(person3.first, person3.last);
+  Promise.all([promise1, promise2, promise3]).then(newArr=>{
+    let arr1,arr2,arr3;
+    [arr1,arr2,arr3]=newArr;
+    console.log(arr1[0].city);
+    console.log(arr2[0].city);
+    console.log(arr3[0].city);
+  }).catch(e=> console.log(e));
+}
 
 // Do not change any of the code below this line.
 module.exports = {
