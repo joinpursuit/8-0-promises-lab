392158dbd0a1bc4cb92539c357afea3b75784bbc
Lakisha J <lakishajohnson@pursuit.org>
2021-09-30 17:36:36 -0400

    Log Three Results

--- src/index.js
+++ src/index.js
@@ -60,7 +60,29 @@ function logTwoResults(person1, person2) {
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities(person1, person2, person3) {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last)
+  .then((person1) => {
+    finder(person2.first, person2.last)
+  .then((person2) => {
+    finder(person3.first, person3.last)
+    .then((person3) => {
+      if(person1, person2, person3) {
+console.log(`${person1[0].city}`) 
+console.log(`${person2[0].city}`)
+console.log(`${person3[0].city}`)
+}
+    }).catch((error) => {
+      console.log(`${error}`)
+    })
+  }).catch((error) => {
+    console.log(`${error}`)
+  })
+}).catch((error) => {
+  console.log(`${error}`)
+})
+
+}
 
 
 
