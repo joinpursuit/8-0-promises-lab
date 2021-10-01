ded8b376e29fb9cadefa191e2286513013311be6
andres ballares <andr3sb@hotmail.com>
2021-09-30 23:29:17 -0400

    working on resolve funtion

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+finder(person.first, person.last).then((element) => {
+    console.log(element)
+}).catch((error) => {
+    console.log(error);
+})
+}
 
 /**
  * logTwoResults()
@@ -22,7 +28,13 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(...person) {
+  person.forEach((ele) => {
+    finder(ele.first, ele.last).then((mango) => {
+      console.log(mango)
+    }).catch((error) => console.log(error))
+  })
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +47,17 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last).then((element) => {
+    console.log(element)
+    finder(person2.first, person2.last).then((element) => {
+      console.log(element)
+      finder(person3.first, person3.last).then((element) => {
+        console.log(element)
+      }).catch((error) => console.log(error))
+    }).catch((error) => console.log(error))
+  }).catch((error) => console.log (error))
+}
 
 // Do not change any of the code below this line.
 module.exports = {
