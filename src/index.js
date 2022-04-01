85caf5325bd0be665590ca9ec24545d65a0b3135
TaryneLeach <taryneleach@gmail.com>
2022-04-01 14:43:57 -0400

    I really missed jest test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/** @format */
+
 // Do not change the line directly below.
-const finder = require("./helpers/finder");
+const finder = require('./helpers/finder');
 
 /**
  * logResult()
@@ -10,7 +12,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+	finder(person.first, person.last)
+		.then((name) => {
+			console.log(name);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
 
 /**
  * logTwoResults()
@@ -22,7 +32,23 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+	finder(person1.first, person1.last)
+		.then((name) => {
+			console.log(name);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+	finder(person2.first, person2.last)
+		.then((name) => {
+			console.log(name);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
 
 /**
  * logThreeResultsCities()
@@ -35,11 +61,41 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+	finder(person1.first, person1.last)
+		.then((name) => {
+			name.forEach((ele) => {
+				console.log(ele.city);
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+	finder(person2.first, person2.last)
+		.then((name) => {
+			name.forEach((ele) => {
+				console.log(ele.city);
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+
+	finder(person3.first, person3.last)
+		.then((name) => {
+			name.forEach((ele) => {
+				console.log(ele.city);
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
 
 // Do not change any of the code below this line.
 module.exports = {
-  logResult,
-  logTwoResults,
-  logThreeResultsCities,
+	logResult,
+	logTwoResults,
+	logThreeResultsCities,
 };
