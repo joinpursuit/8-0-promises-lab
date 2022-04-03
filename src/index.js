930527c182aed8b4df4033a295442c961c04e4ab
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-04-03 16:20:17 -0400

    'Finished'

--- src/index.js
+++ src/index.js
@@ -10,7 +10,26 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+// creates a function with the paramerter an object of person's names 
+function logResult(person) {
+  // invokes the finder function with the person firt and last names 
+  finder(person.first, person.last)
+
+  // if the names are correct .then 
+.then((foundPerson) => {
+
+  // log the persons name 
+  console.log(foundPerson)
+})
+// if the names are incorrect .catch 
+.catch((error) => {
+  // log an error message
+  console.log(error)
+})
+
+
+
+}
 
 /**
  * logTwoResults()
@@ -22,7 +41,39 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+
+// create a function with the parameters person 1 and person2 an object of people names 
+function logTwoResults(person1,person2) {
+
+  // invoke the function finder with the 1st person first and last name match 
+    finder(person1.first, person1.last)
+  // .then if the names match
+    .then((foundPerson) => {
+      // log the first person name 
+      console.log(foundPerson)
+    })
+    // .catch if the names don't match 
+    .catch((error) => { 
+    // log an error message
+      console.log(error)
+    })
+
+      // invoke the finder function with the second person first and last name 
+      finder(person2.first, person2.last)
+
+      // .then if the names match 
+    .then((foundPerson) => {
+      // log the second persons name 
+      console.log(foundPerson)
+    })
+    // .catch if the names don't match 
+    .catch((error) => {   
+      // log an error message 
+      console.log(error)
+
+    })
+
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +86,52 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+// create a function with parameters person 1 2 and 3 objects of people names 
+function logThreeResultsCities(person1, person2, person3) {
+
+  // invoke the finder function with the first person first and last name 
+  finder(person1.first, person1.last)
+
+  // .then if the names match 
+  .then((foundPerson) => {
+    // log the 0 index of the array .key of city
+    console.log(foundPerson[0].city)
+  })
+  // .catch if the names dont match 
+  .catch((error) => { 
+  // log an error message 
+    console.log(error)
+  })
+
+  // invoke the finder function with the second person first and last name 
+    finder(person2.first, person2.last)
+
+    // .then if the names match 
+  .then((foundPerson) => {
+        // log the 0 index of the array .key of city
+    console.log(foundPerson[0].city)
+  })
+    // .catch if the names dont match 
+  .catch((error) => { 
+        // log an error message 
+    console.log(error)
+  })
+
+    // invoke the finder function with the third person first and last name 
+  finder(person3.first, person3.last)
+
+    // .then if the names match 
+  .then((foundPerson) => {
+    // log the 0 index of the array .key of city
+    console.log(foundPerson[0].city)
+  })
+  // .catch if the names dont match 
+  .catch((error) => {   
+    // log an error message
+    console.log(error)
+  })
+  
+}
 
 // Do not change any of the code below this line.
 module.exports = {
