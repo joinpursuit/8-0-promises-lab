02231942ed22abaa58c472e80725a47a8010a6fb
Spencer Simon <spencersimon@pursuit.org>
2022-04-03 18:06:34 -0400

    Promises Lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Do not change the line directly below.
-const finder = require("./helpers/finder");
+const finder = require("./helpers/finder.js");
 
 /**
  * logResult()
@@ -10,7 +10,14 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+finder(person.first, person.last).then((person) => {
+  console.log(person);
+})
+.catch((err) => {
+  console.log(err);
+  });
+}
 
 /**
  * logTwoResults()
@@ -22,7 +29,21 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last).then((person1) => {
+    console.log(person1);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  finder(person2.first, person2.last).then((person2) => {
+    console.log(person2);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +56,28 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last, person1.city).then((person) => {
+    console.log(person[0].city);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
+  finder(person2.first, person2.last, person2.city).then((person) => {
+    console.log(person[0].city);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
+  finder(person3.first, person3.last, person3.city).then((person) => {
+    console.log(person[0].city);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
