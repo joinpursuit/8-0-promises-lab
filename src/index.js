4d4d1857bd308b7e8b883d8de99ca2033869c7ee
arupay <arupay11374@gmail.com>
2022-04-03 11:05:20 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -58,7 +58,29 @@ function logTwoResults(person1, person2) {
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last)
+    .then((element) => {
+      element.forEach((e) => console.log(e.city));
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  finder(person2.first, person2.last)
+    .then((element) => {
+      element.forEach((e) => console.log(e.city));
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  finder(person3.first, person3.last)
+    .then((element) => {
+      element.forEach((e) => console.log(e.city));
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
