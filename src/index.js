aa712da7000339a14dcf31a3af220ea885fed47d
jede <jedebrahma@pursuit.org>
2022-04-03 14:55:16 -0400

    promises kept

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last)
+  .then((foundperson) => {
+    console.log(foundperson);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
 
 /**
  * logTwoResults()
@@ -22,7 +30,22 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last)
+  .then((foundperson1) => {
+    console.log(foundperson1);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  finder(person2.first, person2.last)
+  .then((foundperson2) => {
+    console.log(foundperson2);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +58,29 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last, person1.city)
+  .then((person1) => {
+    console.log(person1[0].city);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  finder(person2.first, person2.last, person2.city)
+  .then((person2) => {
+    console.log(person2[0].city);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  finder(person3.first, person3.last, person3.city)
+  .then((person3) => {
+    console.log(person3[0].city);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
 
 // Do not change any of the code below this line.
 module.exports = {
