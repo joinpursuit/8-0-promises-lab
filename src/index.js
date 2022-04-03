20e601a0dc7027d6ccdd1070af6f031d506dbdf7
Jorge Carrera <JCarrera994@gmail.com>
2022-04-03 14:09:45 -0400

    promises

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+
+function logResult(person) {
+  finder(person.first, person.last)
+    .then((person) => console.log(person))
+    .catch((err) => console.log(err));
+}
 
 /**
  * logTwoResults()
@@ -22,7 +27,24 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+
+function logTwoResults(person1, person2) {
+  // finder(person1.first, person1.last)
+  // .then(personObj => console.log(personObj))
+  // .catch(err => console.log(err))
+
+  // finder(person2.first, person2.last)
+  // .then(personObj => console.log(personObj))
+  // .catch(err => console.log(err))
+
+  const promise1 = finder(person1.first, person1.last);
+  const promise2 = finder(person2.first, person2.last);
+  Promise.all([promise1, promise2])
+    .then((personData) => personData.forEach((x) => console.log(x)))
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +57,29 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+
+function logThreeResultsCities(person1, person2, person3) {
+  // finder(person1.first, person1.last)
+  // .then(personObj => console.log(personObj[0].city))
+  // .catch(err => console.log(err))
+
+  // finder(person2.first, person2.last)
+  // .then(personObj => console.log(personObj[0].city))
+  // .catch(err => console.log(err))
+
+  // finder(person3.first, person3.last)
+  // .then(personObj => console.log(personObj[0].city))
+  // .catch(err => console.log(err))
+
+  const promise1 = finder(person1.first, person1.last);
+  const promise2 = finder(person2.first, person2.last);
+  const promise3 = finder(person3.first, person3.last);
+  Promise.all([promise1, promise2, promise3])
+    .then((personData) => personData.forEach((x) => console.log(x[0].city)))
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
