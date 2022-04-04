9a7c728a87fd42de7c595f3ffdf56b8d71ce2de7
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-04-04 12:38:12 -0400

    Submit  Promise Lab

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last)
+    .then((person) => {
+      console.log(person);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 /**
  * logTwoResults()
@@ -22,7 +30,10 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  logResult(person1);
+  logResult(person2);
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +46,24 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities
+// 3 person objects as parameters
+(person1,person2,person3) {
+  // created a function
+const cityName = (person) => {
+  // used the finder function (promise) to access first and last name keys; if promise is resolved the city key is accesed.
+  finder(person.first, person.last)
+  .then((person) => {
+    console.log(person[0].city);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+cityName(person1); // calling the function to access the key (city), while passing in 3 different person objects.
+cityName(person2);
+cityName(person3);
+}
 
 // Do not change any of the code below this line.
 module.exports = {
