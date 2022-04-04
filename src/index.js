3ad26df25694053a8dc3687e267dc86ac8ffb67e
Fig <bethanytrr@gmail.com>
2022-04-03 23:54:55 -0400

    Lab Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Do not change the line directly below.
-const finder = require("./helpers/finder");
+const finder = require('./helpers/finder');
 
 /**
  * logResult()
@@ -10,7 +10,11 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult({ first, last }) {
+	finder(first, last)
+		.then((success) => console.log(success))
+		.catch((failure) => console.log(failure));
+}
 
 /**
  * logTwoResults()
@@ -22,7 +26,15 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+	finder(person1.first, person1.last)
+		.then((success) => console.log(success))
+		.catch((failure) => console.log(failure));
+
+	finder(person2.first, person2.last)
+		.then((success) => console.log(success))
+		.catch((failure) => console.log(failure));
+}
 
 /**
  * logThreeResultsCities()
@@ -35,11 +47,23 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+	finder(person1.first, person1.last)
+		.then(([{ city }]) => console.log(city))
+		.catch((failure) => console.log(failure));
+
+	finder(person2.first, person2.last)
+		.then(([{ city }]) => console.log(city))
+		.catch((failure) => console.log(failure));
+
+	finder(person3.first, person3.last)
+		.then(([{ city }]) => console.log(city))
+		.catch((failure) => console.log(failure));
+}
 
 // Do not change any of the code below this line.
 module.exports = {
-  logResult,
-  logTwoResults,
-  logThreeResultsCities,
+	logResult,
+	logTwoResults,
+	logThreeResultsCities,
 };
