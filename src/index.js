930cdac6c08cc2bca63a64eac5e2e1b85a40df75
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-04-05 00:49:19 -0400

    logThreeResultsCities()
    Done!

--- src/index.js
+++ src/index.js
@@ -47,7 +47,19 @@ function logTwoResults(person1, person2) {
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last)
+  .then((data) => {console.log(data[0].city)})
+  .catch((error) => {console.log(error)})
+
+  finder(person2.first, person2.last)
+  .then((data) => {console.log(data[0].city)})
+  .catch((error) => {console.log(error)})
+
+  finder(person3.first, person3.last)
+  .then((data) => {console.log(data[0].city)})
+  .catch((error) => {console.log(error)})
+}
 
 // Do not change any of the code below this line.
 module.exports = {
