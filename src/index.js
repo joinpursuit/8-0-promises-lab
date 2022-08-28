27ea67ecc253b7c3ce6c47b746fe5cc1354f4a44
i-guerrero <isaacguerrero@pursuit.org>
2022-08-28 11:54:10 -0400

    Add logThreeResultsCities() to src/index.js

--- src/index.js
+++ src/index.js
@@ -58,7 +58,29 @@ function logTwoResults(person1, person2) {
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last)
+    .then((person1Arr) => {
+      finder(person2.first, person2.last)
+        .then((person2Arr) => {
+          finder(person3.first, person3.last)
+            .then((person3Arr) => {
+              console.log(person1Arr[0].city);
+              console.log(person2Arr[0].city);
+              console.log(person3Arr[0].city);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
