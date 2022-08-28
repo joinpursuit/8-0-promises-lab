51ff1e5bbcc7eefb0b4e87f2c111d4943c5080e0
hector918 <hector_zhong@outlook.com>
2022-08-28 16:03:09 -0400

    submit promises lab

--- src/index.js
+++ src/index.js
@@ -10,8 +10,16 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
-
+async function logResult({first,last}) {
+  try {
+    const da =  await finder(first,last);
+    console.log(da);
+    
+  } catch (error) {
+    console.log(error);
+    
+  }
+}
 /**
  * logTwoResults()
  * ========================
@@ -22,7 +30,15 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(p1,p2) {
+  let tmp = [p1,p2].map(el=>finder(el.first,el.last));
+
+  Promise.all(tmp).then(vals=>{
+    vals.forEach(el=>console.log(el));
+  }).catch(err=>{
+    console.log(err);
+  })
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +51,15 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(p1,p2,p3) {
+  let tmp = Object.values(arguments).map(el=>finder(el.first,el.last));
+
+  Promise.all(tmp).then(vals=>{
+    vals.forEach(el=>console.log(el[0].city));
+  }).catch(err=>{
+    console.log(err);
+  })
+}
 
 // Do not change any of the code below this line.
 module.exports = {
