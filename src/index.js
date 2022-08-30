b6fa04bdaf452488c3e68039adf7197bf02c42a1
pursuit-monique <moniquecorrea@pursuit.orgname>
2022-08-30 18:44:13 -0400

    that last answer is ugly omg

--- src/index.js
+++ src/index.js
@@ -10,7 +10,15 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last)
+    .then((resolve) => {
+      console.log(resolve);
+    })
+    .catch((reject) => {
+      console.log(reject);
+    });
+}
 
 /**
  * logTwoResults()
@@ -22,7 +30,22 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last)
+    .then((resolve) => {
+      console.log(resolve);
+      finder(person2.first, person2.last)
+        .then((resolve) => {
+          console.log(resolve);
+        })
+        .catch((reject) => {
+          console.log(reject);
+        });
+    })
+    .catch((reject) => {
+      console.log(reject);
+    });
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +58,38 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  // [1, 2, 3].forEach((name) => {
+  //   finder(`person${name}.first`, `person${name}.last`)
+  //     .then((resolve) => {
+  //       console.log(resolve);
+  //     })
+  //     .catch((reject) => {
+  //       console.log(reject);
+  //     });
+  // });
+  finder(person1.first, person1.last)
+    .then((resolve) => {
+      console.log(resolve[0].city);
+      finder(person2.first, person2.last)
+        .then((resolve) => {
+          console.log(resolve[0].city);
+          finder(person3.first, person3.last)
+            .then((resolve) => {
+              console.log(resolve[0].city);
+            })
+            .catch((reject) => {
+              console.log(reject);
+            });
+        })
+        .catch((reject) => {
+          console.log(reject);
+        });
+    })
+    .catch((reject) => {
+      console.log(reject);
+    });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
