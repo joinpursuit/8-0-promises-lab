72a11a3c07ac6d760f9e52439fc0568e4d3b0abc
“danielsolaquegit config --global user.email “danielsolaque@pursuit.org <example@email.com>
2022-09-09 07:27:39 -0400

    adding

--- src/index.js
+++ src/index.js
@@ -10,7 +10,26 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+
+function logResult(person, key) {
+  // null coaleshing operator
+
+  // const first = person.first
+  // const last = person.last
+  const { first, last } = person ?? {} //con el doble ?? garantizamos el caso de que cuando sea indefinido tome un objeto vacio.
+
+  if (!first || !last) {
+    console.log("First and Last name are required to search.")
+    return
+  }
+
+  // Finder es una funcion asincrona porque necesita LEER archivos externos que NO DEPENDEN DIRECTAMENTE de nuestra aplicacion.
+  finder(first, last)
+    // THEN es una funcion que se ejecuta una vez la PROMESA se resuelva exitosamente.
+    // Recibe como parametro una funcion del tipo (response) => {}; donde response es la data retornada por la funcion asincrona tras ser resuelta exitosamente.
+    .then(response => console.log(key ? response[0][key] : response))
+    .catch(error => console.log(error))
+}
 
 /**
  * logTwoResults()
@@ -22,7 +41,10 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  logResult(person1)
+  logResult(person2)
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +57,11 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  logResult(person1, 'city')
+  logResult(person2, 'city')
+  logResult(person3, 'city')
+}
 
 // Do not change any of the code below this line.
 module.exports = {
