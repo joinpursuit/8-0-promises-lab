da488e4b5414ceb3aa2415ea436b62ae7102733c
Tech-Kat <katpelle@pursuit.org>
2022-09-11 04:19:59 -0400

    done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last).then(fulfilled => {
+    console.log(fulfilled);
+  }).catch((err) => {
+    console.log(err);
+  })
+}
 
 /**
  * logTwoResults()
@@ -22,7 +28,23 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last).then(fulfilled => {
+    console.log(fulfilled);
+  }).catch((err) => {
+    console.log(err);
+  })
+  
+  finder(person2.first, person2.last).then(fulfilled => {
+    console.log(fulfilled);
+  }).catch((err) => {
+    console.log(err);
+  })
+}
+
+/** same thing as above solution
+  logResult(person1);
+  logResult(person2); */
 
 /**
  * logThreeResultsCities()
@@ -35,7 +57,17 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(...person) {
+  // logResult((person1).city);
+  // logResult((person2).city;
+  // logResult(person3);
+
+  person.forEach(({ first, last }) => {
+    finder(first, last)
+      .then(([{ city }]) => console.log(city))
+      .catch(error => console.log(error));
+  });
+}
 
 // Do not change any of the code below this line.
 module.exports = {
