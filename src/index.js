fa83f691a4b001c836bad8881290b63d2be55175
cjs2408 <carlossuero@pursuit.org>
2022-09-12 16:34:22 -0500

    my cypress is being weird.hoepfully this passes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last)
+  .then((fulfilled) => {
+    console.log(fulfilled);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
 
 /**
  * logTwoResults()
@@ -22,7 +31,19 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last).then(fulfilled => {
+    console.log(fulfilled);
+  }).catch(err => {
+    console.log(err);
+  })
+
+finder(person2.first, person2.last).then(fulfilled => {
+  console.log(fulfilled);
+}).catch(err => {
+  console.log(err);
+})
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +56,14 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(...person) {
+  person.forEach(({first, last}) => {
+    finder(first, last)
+    .then(([{city}]) => console.log(city))
+    .catch(error => console.log(error));
+  }) 
+}
+
 
 // Do not change any of the code below this line.
 module.exports = {
