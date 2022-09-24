9edfda903029d335fbae549e2a045c3406a10c22
ZayLpursuit <Zaylors@pursuit.org>
2022-09-23 20:20:46 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first,person.last)
+    .then(people=>console.log(people))
+    .catch(err=>console.log(err))
+}
 
 /**
  * logTwoResults()
@@ -22,7 +26,15 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1,person2) {
+finder(person1.first,person1.last)
+  .then((firstPerson)=>console.log(firstPerson))
+  .catch((err)=>console.log(err))
+
+finder(person2.first,person2.last)
+.then((secondPerson)=>console.log(secondPerson))
+.catch((err)=>console.log(err))
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +47,19 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1,person2,person3) {
+  finder(person1.first,person1.last)
+  .then((person)=>console.log(person[0].city))
+  .catch((err)=>console.log(err))
+
+  finder(person2.first,person2.last)
+  .then((person)=>console.log(person[0].city))
+  .catch((err)=>console.log(err))
+
+  finder(person3.first,person3.last)
+  .then((person)=>console.log(person[0].city))
+  .catch((err)=>console.log(err))
+}
 
 // Do not change any of the code below this line.
 module.exports = {
