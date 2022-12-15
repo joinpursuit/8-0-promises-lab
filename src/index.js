4b06630b3234145f3326af951b751bf3a0baa654
HaroldF415 <harold.f415@gmail.com>
2022-12-15 01:30:15 -0500

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 // Do not change the line directly below.
 const finder = require("./helpers/finder");
 
+// create a 'helper' function that will help us write less code while still being efficient
+// taking two arguments one being the 'person' and the other 'prop'
+// 'person' is the default argument that is being used in this lab
+// 'prop' is just the property we want printed out instead of the whole 'person'
+function logResultHelper(person, prop){
+
+  // use the default 'finder()' function to look for a person
+  finder(person.first, person.last)
+    .then( person => {
+      // if the 'finder()' function returns a person successfully, the function will then print the 'person' object to the console.
+      // this is unless the 'prop' argument is provided at which point we will print the value of the specified property of the 'person' object
+      console.log( !prop ? person : person[0][prop]);
+    })
+    .catch( error => {
+      console.log(`${error}`);
+    });
+
+};
+
 /**
  * logResult()
  * ========================
@@ -10,7 +29,9 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person){
+  logResultHelper(person);
+}
 
 /**
  * logTwoResults()
@@ -22,7 +43,9 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(...people){
+  people.forEach( person => logResultHelper(person) );
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +58,9 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(...people){
+  people.forEach( person => logResultHelper(person, "city") );
+}
 
 // Do not change any of the code below this line.
 module.exports = {
@@ -43,3 +68,4 @@ module.exports = {
   logTwoResults,
   logThreeResultsCities,
 };
+
