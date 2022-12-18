c64383abe4d4393d4e6d151853045be08ef807f0
JenniferEinstein <jennifereinstein@gmail.com>
2022-12-18 12:23:12 -0500

    second question done and debugged

--- src/index.js
+++ src/index.js
@@ -30,7 +30,19 @@ function logResult(person) {
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+finder(person1.first, person1.last).then((people)=>{
+  console.log(people)
+}).catch((error)=>{
+  console.log(error)
+})
+finder(person2.first, person2.last).then((people)=>{
+  console.log(people)
+}).catch((error)=>{
+  console.log(error)
+})
+}
+
 
 /**
  * logThreeResultsCities()
@@ -43,7 +55,10 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last).then((people)=>{
+    console.log(people.city)
+}
 
 // Do not change any of the code below this line.
 module.exports = {
