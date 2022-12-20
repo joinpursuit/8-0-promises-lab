358fddfd7969056e1f09bf12b2a7e4982658baa6
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-20 13:40:40 -0500

    finish second function

--- src/index.js
+++ src/index.js
@@ -28,7 +28,18 @@ function logResult(person) {
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1,person2) {
+  finder(person1.first,person1.last).then((person) => {
+    console.log(person)
+  }).catch((error)=> {
+    console.log (`${error}`)
+  })
+  finder(person2.first,person2.last).then((person) => {
+    console.log(person)
+  }).catch((error)=> {
+    console.log (`${error}`)
+  })
+}
 
 /**
  * logThreeResultsCities()
@@ -41,7 +52,23 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1,person2,person3) {
+  finder(person1.first,person1.last).then((person) => {
+    console.log(person.city)
+  }).catch((error)=> {
+    console.log (`${error}`)
+  })
+  finder(person2.first,person2.last).then((person) => {
+    console.log(person.city)
+  }).catch((error)=> {
+    console.log (`${error}`)
+  })
+  finder(person3.first,person3.last).then((person) => {
+    console.log(person.city)
+  }).catch((error)=> {
+    console.log (`${error}`)
+  })
+}
 
 // Do not change any of the code below this line.
 module.exports = {
