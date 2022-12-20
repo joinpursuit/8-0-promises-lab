24879aaa8845f01e528dc52a36ea8c104fc805a2
Colin Jaffe <balloonasaurus@gmail.com>
2022-12-20 14:00:04 -0500

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult({ first, last }) {
+  finder(first, last)
+    .then((people) => console.log(people))
+    .catch((error) => console.log(error));
+}
 
 /**
  * logTwoResults()
@@ -22,7 +26,10 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last).then((people) => console.log(people));
+  finder(person2.first, person2.last).then((people) => console.log(people));
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +42,17 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+  finder(person1.first, person1.last).then(([person]) =>
+    console.log(person.city)
+  );
+  finder(person2.first, person2.last).then(([person]) =>
+    console.log(person.city)
+  );
+  finder(person3.first, person3.last).then(([person]) =>
+    console.log(person.city)
+  );
+}
 
 // Do not change any of the code below this line.
 module.exports = {
