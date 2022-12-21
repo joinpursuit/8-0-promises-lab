337d23b4a6230cd01d93403faacdc24bd61d6e05
Ryan Lundy <rlundy1792@gmail.com>
2022-12-21 01:54:19 -0500

    passing all tests

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last)
+  .then((person) => {
+    console.log(person);
+  }) 
+  
+  .catch((error) => {
+    console.log(`${error}`);
+  });
+}
 
 /**
  * logTwoResults()
@@ -22,7 +31,30 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(...people) {
+  // finder(person1.first, person1.last)
+  // .then((person1) => {
+  //   console.log(person1);
+  // })
+  
+  // .catch((error) => {
+  //   console.log(`${error}`);
+  // })
+
+  // finder(person2.first, person2.last)
+  // .then((person2) => {
+  //   console.log(person2);
+  // })
+
+  // .catch((error)=> {
+  //   console.log(`${error}`);
+  // })
+
+  people.forEach(person => {
+    logResult(person);
+  }) 
+
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +67,49 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(...people) {
+
+//   finder (person1.first, person1.last)
+//   .then((person) => {
+//     console.log(person[0].city);
+//   })
+
+//   .catch((error) => {
+//     console.log(`${error}`);
+//   })
+
+//   finder (person2.first, person2.last)
+//   .then((person) => {
+//     console.log(person[0].city);
+//   })
+
+//   .catch((error) => {
+//     console.log(`${error}`);
+//   })
+
+//   finder (person3.first, person3.last)
+//   .then((person) => {
+//     console.log(person[0].city);
+//   })
+
+//   .catch((error) => {
+//     console.log(`${error}`);
+//   })
+
+  people.forEach(person => {
+    finder (person.first, person.last) 
+    .then((personFound) => {
+      console.log(personFound[0].city)
+    })
+
+    .catch ((error) => {
+      console.log(error);
+    })
+  })
+
+}
+
+// console.log(logThreeResultsCities());
 
 // Do not change any of the code below this line.
 module.exports = {
