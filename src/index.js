da34f16ef1aecb863e8297df0a579d4c6d45f03c
MichelBDLC <michelbdlc@gmail.com>
2023-01-10 03:07:48 -0800

    did my best

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ const finder = require("./helpers/finder");
  * @param {Object} person.first - The person's first name.
  * @param {Object} person.last - The person's last name.
  */
-function logResult() {}
+function logResult(person) {
+  finder(person.first, person.last).then((person) => {
+    console.log(person);
+  }).catch((error) => {
+    console.log(error);
+
+  })
+}
 
 /**
  * logTwoResults()
@@ -22,7 +29,25 @@ function logResult() {}
  * @param {Object} person1.last - The person's last name.
  * @param {Object} person2 - A person's names. In the same format as person1.
  */
-function logTwoResults() {}
+function logTwoResults(person1, person2) {
+  finder(person1.first, person1.last).then((person1) => {
+    console.log(person1);
+  }).catch((error) => {
+    console.log(error);
+  })
+
+  finder(person2.first, person2.last).then((person2) => {
+    console.log(person2)
+  }).catch((error) => {
+    console.log(error)
+  });
+  
+  // then((person2) => {
+  //   console.log(person2) 
+  // }).catch((error) => {
+  //   console.log(error);
+  // })
+}
 
 /**
  * logThreeResultsCities()
@@ -35,7 +60,44 @@ function logTwoResults() {}
  * @param {Object} person2 - A person's names. In the same format as person1.
  * @param {Object} person3 - A person's names. In the same format as person1.
  */
-function logThreeResultsCities() {}
+function logThreeResultsCities(person1, person2, person3) {
+
+  finder(person1.first, person1.last).then((person1) => {
+    _convert(person1.line).then((person1) => {
+      let person1city = person1.line[2]
+      console.log(person1city);
+    })
+  }).catch((error) => {
+    console.log(error);
+  })
+
+  finder(person2.first, person2.last).then((person2) => {
+    _convert(person2.line).then((person2) => {
+      let person2city = person2.line[2]
+      console.log(person2city)
+    })
+  }).catch((error) => {
+    console.log(error)
+  })
+
+  finder(person3.first, person3.last).then((person3) => {
+    _convert(person3.line).then((person3) => {
+      let person3city = person3.line[2]
+      console.log(person3city)
+    })
+  }).catch((error) => {
+    console.log(error)
+  });
+
+
+
+
+
+  // _convert(person1.line).then((person1) => {
+  //   let person1City = person1.line[2]
+  //   console.log(person1City)
+  // }).catch
+}
 
 // Do not change any of the code below this line.
 module.exports = {
